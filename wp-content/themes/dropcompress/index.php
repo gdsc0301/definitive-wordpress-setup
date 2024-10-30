c39da7907d6391dcfa7aee2d3f83b937c0d1928c
Guilherme Carvalho <gdsc0301@gmail.com>
2024-10-30 00:45:08 -0300

    fix visuals

--- wp-content/themes/dropcompress/index.php
+++ wp-content/themes/dropcompress/index.php
@@ -13,7 +13,7 @@
 
 get_header(); ?>
 
-<main class="w-full">
+<main class="w-full h-screen grid items-center justify-center">
   <div class="container mx-auto text-center">
     <h1 class="text-xl font-bold">Drop Compress - Blog</h1>
     <p>Under construction</p>
