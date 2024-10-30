389ac2109a4e01003c9770c37843ea987f5d0919
Guilherme Carvalho <gdsc0301@gmail.com>
2024-10-30 00:44:08 -0300

    implement tailwindcss and under construction page

--- wp-content/themes/dropcompress/index.php
+++ wp-content/themes/dropcompress/index.php
@@ -13,32 +13,12 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<?php
-			// Start the loop.
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-			/*
-			 * Previous/next page navigation.
-			 */
-			?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+<main class="w-full">
+  <div class="container mx-auto">
+    <h1>Drop Compress - Blog</h1>
+    <p>Under construction</p>
+  </div>
+</main>
 
 <?php
 get_sidebar();
---
+++ wp-content/themes/dropcompress/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{php,js}",
+    "./index.php",
+    "./functions.php"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
