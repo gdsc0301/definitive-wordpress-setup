cb66f0cfc0b735edc3b13a974cc4b4c1ab6cd145
Guilherme Carvalho <gdsc0301@gmail.com>
2024-10-30 00:47:21 -0300

    under construction

---
+++ wp-content/themes/dropcompress/page-templates/page-under-construction.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package DropCompress
+ */
+
+get_header(); ?>
+
+<main class="w-full h-screen grid items-center justify-center">
+  <div class="container mx-auto text-center">
+    <h1 class="text-xl font-bold">Drop Compress - Blog</h1>
+    <p>Under construction</p>
+  </div>
+</main>
+
+<?php
+get_sidebar();
+get_footer();
